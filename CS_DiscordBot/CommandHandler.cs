f81591fb1bbfbcad16d4d7b340e52e153ff4e032
Владислав Приходько <3885vlad@gmail.com>
2023-02-18 22:00:49 +0200

    Add project files.

---
+++ CS_DiscordBot/CommandHandler.cs
@@ -0,0 +1,75 @@
+﻿using Discord.WebSocket;
+
+using ArtApp.Web;
+
+namespace Tea_and_Tea_Discord_Bot {
+	public class CommandHandler {
+		protected string commandIdentifier;
+		protected SocketMessage message;
+
+		public CommandHandler(string commandIdentifier, SocketMessage message) {
+			this.commandIdentifier = commandIdentifier;
+			this.message = message;
+		}
+
+		public bool IsCommand(string messageText) {
+			return messageText.StartsWith(commandIdentifier);
+		}
+
+		public bool IsCommand(string messageText, out string commandText) {
+			if (!IsCommand(messageText)) {
+				commandText = "";
+				return false;
+			}
+
+			commandText = messageText.Substring(commandIdentifier.Length, messageText.Length - commandIdentifier.Length);
+			return true;
+		}
+
+		public void HandleCommand(SocketMessage message, string command) {
+			Console.WriteLine($"Обробка команди {command}");
+			command = command.Trim().ToLower();
+
+			if (command == "?") {
+				SendMessage(
+					message,
+					"Доступні команди:\n" +
+					"Час\n" +
+					"Автор\n" +
+					"Привіт"
+				);
+			}
+			else if (command == "час") {
+				SendMessage(message, $"{DateTime.Now.ToLongTimeString()}\n{DateTime.Now.ToLongDateString()}");
+				if (DateTime.Now.Hour < 8)
+					SendMessage(message, "Іншими словами час спати");
+			}
+			else if (command == "автор") {
+				SendMessage(message, "<@!883836608963555339> Lite#5625");
+			}
+			else if (command == "привіт") {
+				if (message.Author.Id == 883836608963555339) {
+					SendMessage(message, "Вітаю!");
+					return;
+				}
+				else {
+					SendMessage(message, "Ти хто такий?");
+					return;
+				}
+			}
+			else if (command == "арт") {
+				IsCommand(command, out string subCommand);
+				string[] parameters = subCommand.Split(" ");
+
+				SendMessage(message, WebLoad.GetPictureUrlFromApi("https://api.waifu.pics/sfw/neko", "\"url\":\"([^\"]*)\""));
+			}
+			else {
+				SendMessage(message, "Невідома команда");
+			}
+		}
+
+		protected void SendMessage(SocketMessage message, string text) {
+			message.Channel.SendMessageAsync(text);
+		}
+	}
+}
---
+++ CS_DiscordBot/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Collections.Generic;
+
+using Discord;
+using Discord.WebSocket;
+using Newtonsoft.Json.Linq;
+
+namespace Tea_and_Tea_Discord_Bot {
+	internal class Program {
+		protected DiscordSocketClient client;
+		protected static Random random = new Random(DateTime.Now.Millisecond);
+
+		private static Task Main(string[] args) => new Program().MainAsync();
+
+		public async Task MainAsync() {
+			Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+			string token = File.ReadAllText("Token.txt");
+
+			CreateClientInstance();
+
+			await client.LoginAsync(TokenType.Bot, token);
+			await client.StartAsync();
+
+			await Task.Delay(-1);
+		}
+
+		protected void CreateClientInstance() {
+			client = new DiscordSocketClient(
+				new DiscordSocketConfig {
+					GatewayIntents = GatewayIntents.All,
+					//LogLevel = LogSeverity.Verbose,
+					//AlwaysDownloadUsers = true,
+					//MessageCacheSize = 200
+				}
+			);
+
+			client.MessageReceived += CommandsHandler;
+			client.Log += Log;
+		}
+
+		// Обробники подій
+		private Task CommandsHandler(SocketMessage message) {
+			HandleMessage(message);
+			return Task.CompletedTask;
+		}
+
+		private Task Log(LogMessage msg) {
+			Console.WriteLine(msg.ToString());
+			return Task.CompletedTask;
+		}
+
+		protected void PrintMessageInfo(SocketMessage message) {
+			Console.WriteLine(
+				$"Channel: {message.Channel}\n" +
+				$"Author: {message.Author}\n" +
+				$"Id: {message.Id}\n" +
+				$"EditedTimestamp: {message.EditedTimestamp}\n" +
+				$"CreatedAt: {message.CreatedAt}\n" +
+				$"Application {message.Application}\n" +
+				$"CleanContent: {message.CleanContent}\n" +
+				$"Content: {message.Content}\n"
+			);
+		}
+
+		protected void HandleMessage(SocketMessage message) {
+			if (message.Author.IsBot)
+				return;
+
+			PrintMessageInfo(message);
+
+			CommandHandler commandHandler = new CommandHandler("=", message);
+			if (commandHandler.IsCommand(message.Content, out string command)) {
+				commandHandler.HandleCommand(message, command);
+			}
+		}
+	}
+}
